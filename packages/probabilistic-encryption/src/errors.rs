4f5cc333f1547e9fd0213f5f2171484973cc6113
isk <0xisk@proton.me>
2024-09-16 09:24:48 +0200

    fix: delete the forked version of GM encryption

--- packages/probabilistic-encryption/src/errors.rs
+++
@@ -1,8 +0,0 @@
-/// Error type for generation of private/public keys.
-#[derive(Debug, Fail)]
-pub enum Error {
-    #[fail(display = "length of public key modulus should be greater than 1")]
-    LengthPublicKeyModulus,
-    #[fail(display = "could not generate private/public keys")]
-    CouldNotGenerateKeys,
-}
